c953c480ff8e2c9426475c9f1eb1c88112fcfc94
Ivan Evtushenko <eid20021@gmail.com>
2025-05-14 21:13:31 +0300

    improve code quality

--- src/MathLLMBackend.Core/Services/LlmService/LlmService.cs
+++ src/MathLLMBackend.Core/Services/LlmService/LlmService.cs
@@ -23,31 +23,14 @@ public class LlmService : ILlmService
     public async IAsyncEnumerable<string> GenerateNextMessageStreaming(List<Message> messages, CancellationToken ct)
     {
         var config = _config.Value.ChatModel;
-
-        var client = new ChatClient(
-            model: config.Model,
-            credential: new ApiKeyCredential(config.Token),
-            options: new OpenAIClientOptions() { Endpoint = new Uri(config.Url) });
-        
-
-        var openaiMessages = messages.Select<Message, ChatMessage>(m =>
-            m.MessageType switch
-            {
-                MessageType.User => new UserChatMessage(m.Text),
-                MessageType.Assistant => new AssistantChatMessage(m.Text),
-                MessageType.System => new SystemChatMessage(m.Text),
-                _ => throw new NotImplementedException()
-            }
-        );
-    
+        var client = CreateChatClient(config);
+        var openaiMessages = ConvertMessagesToOpenAIMessages(messages);
         AsyncCollectionResult<StreamingChatCompletionUpdate> completion = client.CompleteChatStreamingAsync(openaiMessages);
-
         await foreach (var chunk in completion)
         {
             ct.ThrowIfCancellationRequested();
             if (chunk.ContentUpdate.Count == 0)
                 break;
-            
             yield return chunk.ContentUpdate[0].Text;
         }
     }
@@ -55,35 +38,36 @@ public class LlmService : ILlmService
     public async Task<string> SolveProblem(string message, CancellationToken ct)
     {
         var config = _config.Value.SolverModel;
-
-        var client = new ChatClient(
-            model: config.Model,
-            credential: new ApiKeyCredential(config.Token),
-            options: new OpenAIClientOptions() { Endpoint = new Uri(config.Url) });
-        
-
+        var client = CreateChatClient(config);
         var openaiMessages = new ChatMessage[]
-            {
-                new SystemChatMessage(_promptService.GetSolverSystemPrompt()),
-                new UserChatMessage(_promptService.GetSolverTaskPrompt(message)),
-            };
-    
+        {
+            new SystemChatMessage(_promptService.GetSolverSystemPrompt()),
+            new UserChatMessage(_promptService.GetSolverTaskPrompt(message)),
+        };
         var completion = await client.CompleteChatAsync(openaiMessages, cancellationToken: ct);
-        
         return completion!.Value.Content[0].Text;
     }
 
     public async Task<string> GenerateNextMessageAsync(List<Message> messages, CancellationToken ct)
     {
         var config = _config.Value.ChatModel;
+        var client = CreateChatClient(config);
+        var openaiMessages = ConvertMessagesToOpenAIMessages(messages);
+        var completion = await client.CompleteChatAsync(openaiMessages, cancellationToken: ct);
+        return completion!.Value.Content[0].Text;
+    }
 
-        var client = new ChatClient(
+    private ChatClient CreateChatClient(ModelConfiguration config)
+    {
+        return new ChatClient(
             model: config.Model,
             credential: new ApiKeyCredential(config.Token),
             options: new OpenAIClientOptions() { Endpoint = new Uri(config.Url) });
-        
+    }
 
-        var openaiMessages = messages.Select<Message, ChatMessage>(m =>
+    private IEnumerable<ChatMessage> ConvertMessagesToOpenAIMessages(List<Message> messages)
+    {
+        return messages.Select<Message, ChatMessage>(m =>
             m.MessageType switch
             {
                 MessageType.User => new UserChatMessage(m.Text),
@@ -92,9 +76,5 @@ public class LlmService : ILlmService
                 _ => throw new NotImplementedException()
             }
         );
-    
-        var completion = await client.CompleteChatAsync(openaiMessages, cancellationToken: ct);
-        
-        return completion!.Value.Content[0].Text;
     }
 }
